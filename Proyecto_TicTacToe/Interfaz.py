bd2d980829c6f49d9e7c1836249fc936d2b3e221
Andres <andrespic4@hotmail.com>
2023-11-02 21:54:04 -0500

    creamos una carpeta que contiene losnuevos archivos y generamos la interfaz

---
+++ Proyecto_TicTacToe/Interfaz.py
@@ -0,0 +1,18 @@
+# creamos la clase donde se van a ejecutar los metodos de la interfaz
+
+
+def interfaz(valor1=1,valor2=2,valor3=3,valor4=4,valor5=5,valor6=6,valor7=7,valor8=8,valor9=9):
+
+    print('+-------+-------+-------+')
+    print('|       |       |       |')
+    print(f'|   {valor1}   |   {valor2}   |   {valor3}   |')
+    print('|       |       |       |')
+    print('+-------|-------+-------+')
+    print('|       |       |       |')
+    print(f'|   {valor4}   |   {valor5}   |   {valor6}   |')
+    print('|       |       |       |')
+    print('+-------+-------+-------+')
+    print('|       |       |       |')
+    print(f'|   {valor7}   |   {valor8}   |   {valor9}   |')
+    print('|       |       |       |')
+    print('+-------|-------+-------+')
---
+++ Proyecto_TicTacToe/Tic_Tac_Toe.py
@@ -0,0 +1,7 @@
+import interfaz as ifz
+
+lista(1,2,3,4,5,6,7,8,9)
+
+lista(X,2,3,4,5,6,7,8,9)
+
+lista(X,2,3,4,5,6,7,O,9)
