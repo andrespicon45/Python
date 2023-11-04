0e27d30940fab5180d8f65159662fc69888e8b48
Andres <andrespic4@hotmail.com>
2023-11-03 21:55:51 -0500

    Creamos condiciones de vitoria, falta empate, continuar y comprobar casillas ocupadas

--- Proyecto_TicTacToe/Tic_Tac_Toe.py
+++ Proyecto_TicTacToe/Tic_Tac_Toe.py
@@ -1,6 +1,10 @@
 from Interfaz import interfaz as ifz
+from random import randrange as random
 
-lista=['x','x','x','x','x','x','x','x','x']
+"""
+Creamos una funcion la cual ayude a asignar a la interfaz los valores guardados en la lista donde se
+almacenan las selecciones
+"""
 
 def asignacion_val(lista):
 
@@ -8,4 +12,61 @@ def asignacion_val(lista):
 
     return impresion
 
-asignacion_val(lista)
+'''
+Creamos una funcion que genere la seleccion de la maquina entre los numeros 1-9
+'''
+
+def sel_maq():
+    return random(1,10)
+
+'''
+Creamos una funcion que reciba la seleccion del usuario
+'''
+
+def sel_usuario():
+
+    while True:
+        x=int(input('Ingrese la celda que desea:'))
+        if x not in range(1,10):
+            print('No es un valor valido.')
+        else:
+            break
+    return x
+
+'''
+Creamos una lista por defecto donde se almacenan las selecciones realizadas
+'''
+
+lista=[1,2,3,4,5,6,7,8,9]
+
+'''
+Creamos las condiciones de victoria, derrota y empate
+'''
+p=[1,2,'O',4,'O',6,'O',8,9]
+def resultado(lista):
+
+    win=((1,2,3),(4,5,6),(7,8,9),(1,4,7),(2,5,8),(3,6,9),(1,5,9),(7,5,3))
+
+    for i in win:
+        if lista[i[0]-1]=='X' and lista[i[1]-1]=='X' and lista[i[2]-1]=="X":
+            print('FELICIDADES!!! Haz vencido a la maquina.')
+            break
+        elif lista[i[0]-1]=='O' and lista[i[1]-1]=='O' and lista[i[2]-1]=="O":
+            print('Haz sido derrotado por SKYNET :(')
+
+resultado(p)
+
+'''
+Creamos el algoritmo del juego
+'''
+
+#u=sel_usuario()
+
+# for val in lista:
+#     indice=lista.index(val)
+#     if u==val:
+#         lista[indice]="X"
+#         print(lista)
+#     else:
+#         print('Elige otra casilla.')
+
--- Prueba_Archivo.py
+++ Prueba_Archivo.py
@@ -1,3 +1,10 @@
 print('esta es una prueba para tratar de subir archivos a GitHub')
 print('conozcamos nuevas funciones en python')
 print('esta es una prueba realizada desde la obra')
+
+lista=[1,2,'X','X','X',4,5,6]
+
+for i in lista:
+    print(i)
+    x=lista.index(i)
+    print(x)
