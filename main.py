6ecff5c5c3e3dd2655fd33689ca22ba220fca061
Andres <andrespic4@hotmail.com>
2024-01-15 21:36:08 -0500

    agg informacion y ejemplo sobre como crear paquetes

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 import sys
 
 sys.path.append("c:\\Users\\andre\\Desktop\\GitHub\\Python\\Modulos")
+sys.path.append("c:\\Users\\andre\\Desktop\\GitHub\\Python\\Ejemplo de paquetes")
 
 import modulo
+import extra.iota
+
+print(extra.iota.FunI())
+
+import extra.good.best.sigma
+from extra.good.best.tau import FunT
+
+print(extra.good.best.sigma.FunS())
+print(FunT())
+
