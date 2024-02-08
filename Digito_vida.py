5224814b2ed4cdbc84f21f502a1aa813808c2835
AndresPicon <andrespic4hotmail.com>
2024-02-08 16:59:33 -0500

    Agregamos la programacion del digito de la vida

--- Digito_vida.py
+++ Digito_vida.py
@@ -1,2 +1,13 @@
 fecha=input('Ingresa tu fecha de nacimiento (solo formato numerico, sin signos):')
-
+total=0
+for caracter in fecha:
+    total+=int(caracter)
+while True:
+    if total<10:
+        break
+    else:
+        fecha=str(total)
+        total=0
+        for caracter in fecha:
+            total+=int(caracter)
+print(total)
